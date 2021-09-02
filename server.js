00f9e793641ce62e2cafd40a0f2bc824645782bb
brookemadison <brookeperry611@gmail.com>
2021-09-02 19:56:03 -0400

    created routes to serve other pages

--- server.js
+++ server.js
@@ -14,10 +14,24 @@ app.get('/api/notes', (req, res) => {
   res.json(results);
 });
 
-
 app.post('/api/notes', (req, res) => {
   console.log(req.body);
   res.json(req.body);
+});
+
+//route to index.html (home page)
+app.get('/index', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+//route to notes.html (note page)
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+//wildcard route
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 app.listen(PORT, () => {
